f2360c946e792c585261aca26612502dc2303445
mcll <maclon@canway.net>
2023-07-31 20:35:35 +0800

    bcs netservice controller init

---
+++ bcs-services/bcs-netservice-controller/api/v1/bcsnetip_types.go
@@ -0,0 +1,73 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// BCSNetIPSpec defines the desired state of BCSNetIP
+type BCSNetIPSpec struct {
+	// 所属Pool
+	Pool    string `json:"pool,omitempty"`
+	Mask    int    `json:"mask,omitempty"`
+	Gateway string `json:"gateway,omitempty"`
+
+	// 容器ID
+	Container string `json:"container,omitempty"`
+	// 对应主机信息
+	Host string `json:"host"`
+}
+
+// BCSNetIPStatus defines the observed state of BCSNetIP
+type BCSNetIPStatus struct {
+	// Active --已使用，Available --可用, Reserved --保留
+	Status       string `json:"status,omitempty"`
+	PodName      string `json:"podName,omitempty"`
+	PodNamespace string `json:"podNamespace,omitempty"`
+	CreateTime   string `json:"createTime,omitempty"`
+	UpdateTime   string `json:"updateTime,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:resource:scope=Cluster
+//+kubebuilder:subresource:status
+
+// BCSNetIP is the Schema for the bcsnetips API
+type BCSNetIP struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   BCSNetIPSpec   `json:"spec,omitempty"`
+	Status BCSNetIPStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// BCSNetIPList contains a list of BCSNetIP
+type BCSNetIPList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []BCSNetIP `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&BCSNetIP{}, &BCSNetIPList{})
+}
---
+++ bcs-services/bcs-netservice-controller/api/v1/bcsnetpool_types.go
@@ -0,0 +1,72 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// BCSNetPoolSpec defines the desired state of BCSNetPool
+type BCSNetPoolSpec struct {
+	// 网段掩码
+	Mask int `json:"mask,omitempty"`
+	// 网段网关
+	Gateway string `json:"gateway,omitempty"`
+	// 对应主机列表
+	Hosts []string `json:"hosts,omitempty"`
+	// 可用的IP
+	AvailableIPs []string `json:"availableIPs,omitempty"`
+	// 保留的IP
+	ReservedIPs []string `json:"reservedIPs,omitempty"`
+}
+
+// BCSNetPoolStatus defines the observed state of BCSNetPool
+type BCSNetPoolStatus struct {
+	// Initializing --初始化中，Normal --正常
+	Status     string `json:"status,omitempty"`
+	CreateTime string `json:"createTime,omitempty"`
+	UpdateTime string `json:"updateTime,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:resource:scope=Cluster
+//+kubebuilder:subresource:status
+
+// BCSNetPool is the Schema for the bcsnetpools API
+type BCSNetPool struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   BCSNetPoolSpec   `json:"spec,omitempty"`
+	Status BCSNetPoolStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// BCSNetPoolList contains a list of BCSNetPool
+type BCSNetPoolList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []BCSNetPool `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&BCSNetPool{}, &BCSNetPoolList{})
+}
